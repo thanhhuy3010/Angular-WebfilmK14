9f9118beacb9ed63911ec3f12958bb26ef39488a
Nhật Tiến Châu <nhattien24071996@gmail.com>
2019-02-02 13:03:01 +0700

    create filmDetail filmBooking

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
 import { HomePageComponent } from './home-layout/home-page/home-page.component';
+import { FilmDetailComponent } from './home-layout/film-detail/film-detail.component';
+import { FilmBookingComponent } from './home-layout/film-booking/film-booking.component';
 
 @NgModule({
-  declarations: [HomeLayoutComponent, HomePageComponent],
+  declarations: [HomeLayoutComponent, HomePageComponent, FilmDetailComponent, FilmBookingComponent],
   imports: [
     CommonModule,
   ],
