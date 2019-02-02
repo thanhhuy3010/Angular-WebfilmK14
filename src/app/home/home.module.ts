105df443be0c30fd0096fe69797c826635196183
Nhật Tiến Châu <nhattien24071996@gmail.com>
2019-02-02 11:37:57 +0700

    create homeModule

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeLayoutComponent } from './home-layout/home-layout.component';
+
+@NgModule({
+  declarations: [HomeLayoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
