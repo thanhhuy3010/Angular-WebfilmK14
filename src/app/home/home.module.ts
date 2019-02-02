165e7e404f58178ba969e6fb0e06fdc01b142e66
thanhhuy3010 <huyhuythanh3010@gmail.com>
2019-02-02 12:16:59 +0700

    create thanhhuy branch and create homepage component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
+import { HomePageComponent } from './home-layout/home-page/home-page.component';
 
 @NgModule({
-  declarations: [HomeLayoutComponent],
+  declarations: [HomeLayoutComponent, HomePageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [
+    HomeLayoutComponent,HomePageComponent
   ]
 })
 export class HomeModule { }
