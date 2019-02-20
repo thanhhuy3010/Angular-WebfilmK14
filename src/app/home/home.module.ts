a0852e6da51c333ebc72dfe13447f009e3d2d210
Nhật Tiến Châu <nhattien24071996@gmail.com>
2019-02-20 10:24:31 +0700

    version 1

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
 import { HomePageComponent } from './home-layout/home-page/home-page.component';
 import { FilmDetailComponent } from './home-layout/film-detail/film-detail.component';
 import { FilmBookingComponent } from './home-layout/film-booking/film-booking.component';
+import { SliderComponent } from './home-layout/slider/slider.component';
+import { NavbarComponent } from './home-layout/navbar/navbar.component';
+import { QuickBookingComponent } from './home-layout/quick-booking/quick-booking.component';
+import { FormsModule } from '@angular/forms';
+import { MovieTypeComponent } from './home-layout/movie-type/movie-type.component';
+import { MoviePlayingComponent } from './home-layout/movie-playing/movie-playing.component';
+import { MovieComingsoonComponent } from './home-layout/movie-comingsoon/movie-comingsoon.component';
+import { ItemMovieComponent } from './home-layout/item-movie/item-movie.component';
+
+import { SwiperModule } from 'angular2-useful-swiper';
+
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MaterialModule } from "../_core/share/Module/material/material.module";
 
 @NgModule({
-  declarations: [HomeLayoutComponent, HomePageComponent, FilmDetailComponent, FilmBookingComponent],
+  declarations: [HomeLayoutComponent, HomePageComponent, FilmDetailComponent, FilmBookingComponent,  SliderComponent, NavbarComponent, QuickBookingComponent, MovieTypeComponent, MoviePlayingComponent, MovieComingsoonComponent, ItemMovieComponent],
   imports: [
-    CommonModule,
+    CommonModule,FormsModule,SwiperModule
   ],
   exports: [
     HomeLayoutComponent,HomePageComponent
